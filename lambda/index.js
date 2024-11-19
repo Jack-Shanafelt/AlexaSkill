8f32f5169fbc5438a2054efc9e5f17a1c91fc820
Jack-Shanafelt <Shanafeltj1@gmail.com>
2024-11-19 16:53:05 -0700

    fix

--- lambda/index.js
+++ lambda/index.js
@@ -91,6 +91,21 @@ const WellnessBoostIntentHandler = {
     }
 };
 
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+
 const HelpIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
